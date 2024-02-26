9cab3d333e1c746eb133c784e789c7f8a99813d2
unknown <abinashbhattarai3@gmail.com>
2024-02-26 12:16:58 +0545

    if/else and casting

---
+++ Chapter5/TypeConversion.java
@@ -0,0 +1,13 @@
+//type conversion and casting
+
+class TypeConversion
+{
+  public static void main(String[] args)
+  {
+    int a = 12;
+    float b;
+    b = (float)a;
+
+    System.out.println(b);
+  }
+}
---
+++ Chapter6_11/IfElseOperators.java
@@ -0,0 +1,19 @@
+//If Else and operators
+
+class IfElseOperators
+{
+  public static void main(String[] args)
+  {
+    int a = 22;
+    int b = 33;
+
+    if(a>b)
+    {
+      System.out.println("a is greater");
+    }
+    else
+    {
+      System.out.println("b is greater");
+    }
+  }
+}
