5c79cb56bc98c3c3173775d3974e9f1315f8dcb9
AbinashBhattarai <abinashbhattarai3@gmail.com>
2024-07-16 22:11:34 +0545

    recursion/hashing

---
+++ S1L5-Recursion/Challenges/Q6/Program.cs
@@ -0,0 +1,93 @@
+﻿// Q: Reverse an array
+
+
+// Sol: using new array
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         int[] arr = [1,2,3,4,5,6];
+//         Reverse(arr);
+//     }
+
+//     public static void Reverse(int[] arr)
+//     {
+//         int[] newArr = new int[arr.Length];
+
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//            newArr[i] = arr[arr.Length - i - 1];
+//         }
+
+//         foreach (int i in newArr)
+//         {
+//             Console.Write(i + " ");
+//         }
+//     }
+// }
+
+
+
+// Sol: Using two pointer approach
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         int[] arr = [1,2,3,4,5,6];
+//         Reverse(arr);
+//     }
+
+//     public static void Reverse(int[] arr)
+//     {
+
+//         int start = 0;
+//         int end = arr.Length - 1;
+//         int temp;
+
+//         while(start < end)
+//         {
+//             temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             end--;
+//         }
+
+//         foreach (int i in arr)
+//         {
+//             Console.Write(i + " ");
+//         }
+//     }
+// }
+
+
+// Sol: Using recursion
+public class Program
+{
+    public static void Main()
+    {
+        int[] arr = [1,5,8,7,9];
+        Reverse(arr, 0, arr.Length-1);
+
+        foreach (int i in arr)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    public static void Reverse(int[] arr, int start, int end)
+    {
+
+        if(start > end)
+        {
+            return;
+        }
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        Reverse(arr, start + 1, end - 1);
+    }
+}
+
---
+++ S1L5-Recursion/Practice/Hashing/Program.cs
@@ -0,0 +1,96 @@
+﻿// Find the number of occurence
+class Program{
+    public static void Main(){
+        
+        // int[] arr = [1,3,2,4,1,3,5];
+        // int[] query = [1,2,3,4,5];
+        // Check(arr, query);
+        
+        string s = "abfgabdd";
+        char[] query = ['a', 'b', 'd', 'f', 'g'];
+        Check(s, query);
+    }
+
+    // Numbers (Normal hashing approach)
+    /*
+    public static void Check(int[] a, int[] q){
+        int[] count = new int[a.Length+1];
+
+
+        for(int i=0; i<a.Length; i++){
+            count[a[i]]+=1; 
+        }
+
+
+        foreach(int elem in q){
+            Console.WriteLine(count[elem]);
+        }
+        
+    }
+    */
+
+    // String (Normal hashing approach)
+    /*
+    public static void Check(string s, char[] q){
+        int[] count = new int[26];
+
+
+        for(int i=0; i<s.Length; i++){
+            count[s[i]-'a']+=1; 
+        }
+
+
+        foreach(int elem in q){
+            Console.WriteLine(count[elem - 'a']);
+        }
+    }
+    */
+
+
+    // Using Dictionary (Numbers)
+    /*
+    public static void Check(int[] a, int[] q){
+
+        Dictionary<int, int> count = [];
+
+        foreach (int i in a)
+        {
+            if (!count.ContainsKey(i))
+            {
+                count.Add(i, 1);
+            }
+            else
+            {
+                count[i]++;
+            }
+        }
+
+
+        foreach (int elem in q)
+        {
+            Console.WriteLine(count[elem]);
+        }
+    }
+    */
+
+    // Using Dictionary (String)
+
+    public static void Check(string s, char[] q){
+        Dictionary<char, int> count = [];
+
+
+        foreach(char c in s){
+            if(!count.ContainsKey(c)){
+                count.Add(c, 1);
+            }
+            else{
+                count[c]++;
+            }
+        }
+
+
+        foreach(char ch in q){
+            Console.WriteLine(count[ch]);
+        }
+    }
+}
