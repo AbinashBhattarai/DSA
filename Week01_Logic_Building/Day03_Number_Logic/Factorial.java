ec5c556848c924058a3d2d03c248bb828f17172c
Abinash Bhattarai <abinashbhattarai3@gmail.com>
2026-09-07 13:50:47 +0545

    Week1 Day3: Number_Logic

---
+++ Week01_Logic_Building/Day03_Number_Logic/Factorial.java
@@ -0,0 +1,25 @@
+/*
+Question: Find the factorial of a given number
+
+Explanation:
+- use loop and multiply numer from 1 to n
+
+TC = O(n)
+SC = O(1)
+ */
+
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        int num = 9;
+        int fact = 1;
+
+        for(int i=1; i<=num; i++)
+        {
+            fact *= i;
+        }
+        System.out.println("The factorial of " + num + " is " + fact);
+    }
+}
